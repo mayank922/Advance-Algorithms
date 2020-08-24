888d43c2231cafdb3ca1ba6702db6051696e4b64
mayank922 <60442308+mayank922@users.noreply.github.com>
2020-08-24 20:39:57 +0530

    Update N_Queens.cpp

--- Bactracking/N_Queens.cpp
+++ Bactracking/N_Queens.cpp
@@ -1,3 +1,7 @@
+/*You are given N, and for a given N x N chessboard, find a way to place N queens such that no queen can attack any other queen on the chess board.
+A queen can be killed when it lies in the same row, or same column, or the same diagonal of any of the other queens. You have to print all such configurations.*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int board[11][11];
@@ -71,7 +75,8 @@ void placequeens(int n)
 }
 
 int main()
-{
-	placequeens(10);
+{       int n;
+        cin>>n;
+	placequeens(n);
 	return 0;
 }
